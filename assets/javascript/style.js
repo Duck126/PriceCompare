5c14254c61c065caf2c537758c4525520c76a50d
Chitica <liseth_gdl_86@hotmail.com>
2018-03-20 00:49:44 -0500

    added styles

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -1,35 +1,7 @@
-////////PAGE Slide Function////////
-$(document).ready(function(){
-    $("a").on('click', function(event) {
 
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-
-        $('html, body').animate({
-
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-    
-          window.location.hash = hash;
-        });
-      } 
-    });
-});
-
-//////Mobile Nav//////////////
-
-$(document).ready(function(){
-  nav = $(".toggle");
-  menuList = $(".nav");
-  listItem = $("li");
-
-  nav.click(function(){
-    menuList.toggleClass("active");
-  
-    
-
-  })
-})
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
 
 
