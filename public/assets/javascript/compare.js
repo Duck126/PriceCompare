137f2648b1b303a8dd635a98bac7de098bc48ea4
Duck126 <jduck1133@gmail.com>
2018-03-23 16:06:30 -0500

    Initial Working App

--- public/assets/javascript/compare.js
+++ public/assets/javascript/compare.js
@@ -1,16 +1,3 @@
-/*$(document).ready(function () {
-
-    var ready = function () {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCttHUx3kXeV6l4jUMIw8jL5ZhrNhafsO0&libraries=places&callback=initMap';
-        script.text = 'async defer'
-
-        document.body.appendChild(script);
-    }
-    ready();
-});*/
-
 function initMap() {
     var austin = {
         lat: 30.2672,
@@ -29,207 +16,134 @@ function initMap() {
         location: austin,
         radius: 1000000,
         type: ['music']
-    }, callback);
+    }, function () {});
 }
+
 $(document).ready(function () {
     //alert("working");
 
-    /*You code */
-    // This example requires the Places library. Include the libraries=places
-    // parameter when you first load the API. For example:
-    // 
-    //$("#submit-button").on("click", function () {
-
-    function getLocationData(click) {
-        eventListener('#submit-button', 'click', function () {
-            var LocationData = JSON.parse(localStorage.getItem('location'));
-            console.log(localStorage.eventLoc);
-        });
+    var config = {
+        apiKey: "AIzaSyBNSBALDx4nwVVXPZmiHCgC3_Nvvbg-Vc8",
+        authDomain: "price-compare-project-73cd9.firebaseapp.com",
+        databaseURL: "https://price-compare-project-73cd9.firebaseio.com",
+        projectId: "price-compare-project-73cd9",
+        storageBucket: "price-compare-project-73cd9.appspot.com",
+        messagingSenderId: "1037112672846"
     };
+    firebase.initializeApp(config);
 
-    function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
+    var dataB = firebase.database();
+    var searchData = [];
 
-            }
-        }
-    };
+    $("#submit-button").on("click", function (e) {
+        e.preventDefault();
+        $("#inputs").empty();
+        var userKeyword = $("#keyword").val().trim();
+        var locAdd = $("#loc-address").val().trim();
 
-    function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
+        $("#1mi").val("1mi");
+        $("#5mi").val("5mi");
+        $("#10mi").val("10mi");
+        $("#20mi").val("20mi");
 
-        });
-        //console.log(place.geometry);
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.setContent(place.name);
-            infowindow.open(map, this);
-        });
-    };
-    //});
+        $("#today").val("today");
+        $("#tomorrow").val("tomorrow");
+        $("#this-week").val("this_week");
+        $("#next-week").val("next_week");
+        $("#this-month").val("this_month");
+        $("#next-month").val("next_month");
 
 
-});
+        var locWithin = $("#loc-with").val();
+        var startDate = $("#start-date").val();
 
+        console.log("kw", userKeyword);
+        console.log("loca", locAdd);
+        console.log("locw", locWithin);
+        console.log("date", startDate);
 
-
-var config = {
-    apiKey: "AIzaSyBNSBALDx4nwVVXPZmiHCgC3_Nvvbg-Vc8",
-    authDomain: "price-compare-project-73cd9.firebaseapp.com",
-    databaseURL: "https://price-compare-project-73cd9.firebaseio.com",
-    projectId: "price-compare-project-73cd9",
-    storageBucket: "price-compare-project-73cd9.appspot.com",
-    messagingSenderId: "1037112672846"
-};
-
-firebase.initializeApp(config);
-
-var dataB = firebase.database();
-var searchCounter = 0;
-
-
-//$("#action-1").click(function(e){
-//do something
-//e.preventDefault();
-// });
-
-
-$("#submit-button").on("click", function () {
-
-    $("#inputs").empty();
-    var userKeyword = $("#keyword").val().trim();
-    var locAdd = "austin"; //$("#loc-address");
-    var locWithin = "10mi"; //$("#loc-within");
-    var startDate = "this_month"; //$("#start-date");
-    console.log("kw", userKeyword);
-    console.log("loca", locAdd);
-    console.log("locw", locWithin);
-    console.log("date", startDate);
-
-    //you can test this URL by actually going to it https://www.eventbriteapi.com/v3/events/search/?q=crawfish+boil&location.address=austin&location.within=10mi&start_date.keyword=this_month&token=Y54FW5RHAXG43FRWR4QJ&expand=venue
-    //the above is an example, feel free to play around with it. their api is cool.
-    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + userKeyword +
-        "&location.address=" + locAdd +
-        "&location.within=" + locWithin +
-        "&start_date.keyword=" + startDate +
-        "&token=Y54FW5RHAXG43FRWR4QJ&expand=venue";
-    //console.log(queryURL);
-
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function (response) {
-            console.log(response, map);
-
-            //dataB.ref().push(response);
-            //var results = response.data();
-            //console.log(results);
-
-            var searchDataLong = [];
-            var searchDataLat = [];
-            let googleinput = [];
-
-            for (var i = 0; i < response.events.length; i++) {
-                let thisEvent = response.events[i];
-
-                var marker = new google.maps.Marker({
-                    position: {
-                        lat: parseFloat(thisEvent.venue.address.latitude),
-                        lng: parseFloat(thisEvent.venue.address.longitude)
-                    },
-                    label: "Marker",
-                    map: map,
-                    title: 'Hello World!'
-                });
-
-
-                /*let item = {
-                    name: response.events[i].name.text,
-                    startDateAndTime: response.events[i].start.local,
-                    venueName: response.events[i].venue.name,
-                    venueAddress: response.events[i].venue.address,
-                }
-                let location = {
-                    latitude: response.events[i].venue.address.latitude,
-                    longitude: response.events[i].venue.address.longitude,
-                }*/
-
-
-
-
-                //console.log(localStorage.eventLoc.location);
-
-                //searchData.push(item);
-                // googleInput.push(location);
-                //console.log(location);
-
-            };
-            //dataB.ref().push(searchData);
-        });
-
-    /* function getGoogleData {
-            var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + userKeyword +
+        //you can test this URL by actually going to it https://www.eventbriteapi.com/v3/events/search/?q=crawfish+boil&location.address=austin&location.within=10mi&start_date.keyword=this_month&token=Y54FW5RHAXG43FRWR4QJ&expand=venue
+        //the above is an example, feel free to play around with it. their api is cool.
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + userKeyword +
             "&location.address=" + locAdd +
             "&location.within=" + locWithin +
             "&start_date.keyword=" + startDate +
             "&token=Y54FW5RHAXG43FRWR4QJ&expand=venue";
-            //console.log(queryURL);
-        
-            $.ajax({
+        //console.log(queryURL);
+
+        $.ajax({
                 url: queryURL,
                 method: "GET"
             })
-                .then(function (response) {
-
-
-                    
-        }*/
+            .then(function (response) {
+                //console.log(response, map);
 
 
 
-    //dataB.ref().on('value', function snapshotToArray(childSnapshot) {
-    //  console.log(childSnapshot);
-    //let returnArr = [];
+                let googleInput = [];
+                let number = 0;
+                var searchDataLong = [];
+                var searchDataLat = [];
+                let googleinput = [];
 
-    //childSnapshot.forEach(function(childSnapshot) {
-    //  var displayItem = childSnapshot.val();
-    //displayItem.key = childSnapshot.key;
+                for (var i = 0; i < 11; i++) {
 
-    //returnArr.push(displayItem);
+                    let item = {
+                        name: response.events[i].name.text,
+                        startDateAndTime: response.events[i].start.local,
+                        venueName: response.events[i].venue.name,
+                        venueAddress: response.events[i].venue.address,
+                    }
 
-    //});
-    //console.log(returnArr);
-    //return returnArr;
-
-    //});
-
-
-});
+                    let location = {
+                        latitude: response.events[i].venue.address.latitude,
+                        longitude: response.events[i].venue.address.longitude,
+                    }
 
 
-//for (var i = 0; i < 10; i++) {
 
-//$("#search-results").append("<ul><li>" + returnArr[i].name + "</li><li>" + returnArr[i].startDateAndTime + "</li><li>" + returnArr[i].venueName +  "</li><li>" + returnArr[i].venueAddress + "</li></ol>");
-//}
+                    console.log(item);
+                    console.log(searchData);
+                    let time = moment(item.startDateAndTime).format("dddd, MMMM Do, h:mm a");
+                    $("#list-anchor").append("<h4 id='list-item-" + number++ + "'>" + item.name + "</h4><p>" + time + "<br >" + item.venueName + "</p <hr>");
+                    $("#list-display").append("<a class='list-group-item list-group-item-action' href='list-item-" + number + "'> event </a>");
+                    console.log(number, "number");
+
+                    let thisEvent = response.events[i];
+
+                    var marker = new google.maps.Marker({
+                        position: {
+                            lat: parseFloat(thisEvent.venue.address.latitude),
+                            lng: parseFloat(thisEvent.venue.address.longitude)
+                        },
+                        label: "Marker",
+                        map: map,
+                        title: 'Hello World!'
+                    });
+
+
+
+                    searchData.push(item);
+                    googleInput.push(location);
+
+                };
+                dataB.ref().push(searchData);
+            });
+
+    });
+
+})
+
+
+
 //AIzaSyDdHwuSi6AbQZbktOwjTx4Nz3kINmLI2fw
-
-
 //AIzaSyCttHUx3kXeV6l4jUMIw8jL5ZhrNhafsO0 - Google APIKey
 
 //https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=YOUR_API_KEY
 //https://maps.googleapis.com/maps/api/place/details/output?parameters
+//*var urlPlaces = "https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDdHwuSi6AbQZbktOwjTx4Nz3kINmLI2fw";
 
 
-/*var urlPlaces = "https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDdHwuSi6AbQZbktOwjTx4Nz3kINmLI2fw";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
->>>>>>> f3754ce545faaa7b8379f0aed548f1bbe5e8f40b
-
-});*/
+//dataB.ref().push(response);
+//var results = response.data();
+//console.log(results);
